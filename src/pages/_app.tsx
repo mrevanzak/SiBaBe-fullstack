89d76a8c1ccf1014768fc88dc85e7ef74e6febc0
mrevanzak <mrevanzak@gmail.com>
2023-08-20 17:53:40 +0700

    refactor(web): remove redux entirely (fck redux)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,18 @@ import { MantineProvider } from '@mantine/core';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import '@/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { client, queryClient, rspc } from '@/lib/rspc';
 
-import { persistor, wrapper } from '@/redux';
-
 /**
  * !STARTERCONF info
  * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
  */
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <rspc.Provider client={client} queryClient={queryClient}>
       <ClerkProvider {...pageProps}>
@@ -29,15 +26,11 @@ function MyApp({ Component, pageProps }: AppProps) {
             colorScheme: 'light',
           }}
         >
-          <PersistGate loading={null} persistor={persistor}>
-            <Component {...pageProps} />
-            <ToastContainer position='top-center' />
-          </PersistGate>
+          <Component {...pageProps} />
+          <ToastContainer position='top-center' />
         </MantineProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </ClerkProvider>
     </rspc.Provider>
   );
 }
-
-export default wrapper.withRedux(MyApp);
